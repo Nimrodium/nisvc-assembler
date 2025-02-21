a1a2cb80c5dd655684b16fb85e24e323e53bf430
Nimrodium <kylewillmann@gmail.com>
2025-02-20 23:50:52 -0600

    added some of the basic parsing logic and defined operation table

--- src/data.rs
+++ src/data.rs
@@ -13,18 +13,20 @@ pub enum Severity {
 
 pub enum AssemblyErrorCode {
     InvalidOpcodeDefinition,
+    UnrecognizedOpcode,
+    IncorrectNumberOfOperands,
 }
 
 pub struct AssemblyError {
-    code: AssemblyErrorCode,
-    reason: Option<String>,
-    severity: Severity,
+    pub code: AssemblyErrorCode,
+    pub reason: Option<String>,
+    pub severity: Severity,
 }
 
-struct OpcodeEntry {
-    code: usize,
-    fields: usize,
-    field_types: Vec<InterType>,
+pub struct OpcodeEntry {
+    pub code: usize,
+    pub fields: usize,
+    pub field_types: Vec<InterType>,
 }
 impl OpcodeEntry {
     fn new(code: usize, fields: usize, field_types: Vec<InterType>) -> Result<Self, AssemblyError> {
@@ -45,12 +47,191 @@ impl OpcodeEntry {
     }
 }
 
-struct OpcodeTable {
+pub struct OpcodeTable {
     table: HashMap<String, OpcodeEntry>,
 }
 
 impl OpcodeTable {
-    fn build_table() -> Result<Self, AssemblyError> {}
+    pub fn build_table() -> Result<Self, AssemblyError> {
+        let table: HashMap<String, OpcodeEntry> = HashMap::from([
+            ("nop".to_string(), OpcodeEntry::new(0x00, 0, vec![])?),
+            (
+                "mov".to_string(),
+                OpcodeEntry::new(0x01, 2, vec![InterType::Reg, InterType::Reg])?,
+            ),
+            (
+                "movim".to_string(),
+                OpcodeEntry::new(0x02, 2, vec![InterType::Reg, InterType::Imm])?,
+            ),
+            (
+                "load".to_string(),
+                OpcodeEntry::new(
+                    0x03,
+                    3,
+                    vec![InterType::Reg, InterType::Reg, InterType::Reg],
+                )?,
+            ),
+            (
+                "store".to_string(),
+                OpcodeEntry::new(
+                    0x04,
+                    3,
+                    vec![InterType::Reg, InterType::Reg, InterType::Reg],
+                )?,
+            ),
+            (
+                "add".to_string(),
+                OpcodeEntry::new(
+                    0x05,
+                    3,
+                    vec![InterType::Reg, InterType::Reg, InterType::Reg],
+                )?,
+            ),
+            (
+                "sub".to_string(),
+                OpcodeEntry::new(
+                    0x06,
+                    3,
+                    vec![InterType::Reg, InterType::Reg, InterType::Reg],
+                )?,
+            ),
+            (
+                "mult".to_string(),
+                OpcodeEntry::new(
+                    0x07,
+                    3,
+                    vec![InterType::Reg, InterType::Reg, InterType::Reg],
+                )?,
+            ),
+            (
+                "div".to_string(),
+                OpcodeEntry::new(
+                    0x08,
+                    3,
+                    vec![
+                        InterType::Reg,
+                        InterType::Reg,
+                        InterType::Reg,
+                        InterType::Reg,
+                    ],
+                )?,
+            ),
+            (
+                "or".to_string(),
+                OpcodeEntry::new(
+                    0x09,
+                    3,
+                    vec![InterType::Reg, InterType::Reg, InterType::Reg],
+                )?,
+            ),
+            (
+                "xor".to_string(),
+                OpcodeEntry::new(
+                    0x0a,
+                    3,
+                    vec![InterType::Reg, InterType::Reg, InterType::Reg],
+                )?,
+            ),
+            (
+                "and".to_string(),
+                OpcodeEntry::new(
+                    0x0b,
+                    3,
+                    vec![InterType::Reg, InterType::Reg, InterType::Reg],
+                )?,
+            ),
+            (
+                "not".to_string(),
+                OpcodeEntry::new(0x0c, 2, vec![InterType::Reg, InterType::Reg])?,
+            ),
+            (
+                "shl".to_string(),
+                OpcodeEntry::new(
+                    0x0d,
+                    3,
+                    vec![InterType::Reg, InterType::Reg, InterType::Reg],
+                )?,
+            ),
+            (
+                "shr".to_string(),
+                OpcodeEntry::new(
+                    0x0e,
+                    3,
+                    vec![InterType::Reg, InterType::Reg, InterType::Reg],
+                )?,
+            ),
+            (
+                "rotl".to_string(),
+                OpcodeEntry::new(
+                    0x0f,
+                    3,
+                    vec![InterType::Reg, InterType::Reg, InterType::Reg],
+                )?,
+            ),
+            (
+                "rotr".to_string(),
+                OpcodeEntry::new(
+                    0x10,
+                    3,
+                    vec![InterType::Reg, InterType::Reg, InterType::Reg],
+                )?,
+            ),
+            (
+                "neg".to_string(),
+                OpcodeEntry::new(0x11, 2, vec![InterType::Reg, InterType::Reg])?,
+            ),
+            (
+                "jmp".to_string(),
+                OpcodeEntry::new(0x12, 1, vec![InterType::Addr])?,
+            ),
+            (
+                "jifz".to_string(),
+                OpcodeEntry::new(0x13, 2, vec![InterType::Reg, InterType::Addr])?,
+            ),
+            (
+                "jifnz".to_string(),
+                OpcodeEntry::new(0x14, 2, vec![InterType::Reg, InterType::Addr])?,
+            ),
+            (
+                "pr".to_string(),
+                OpcodeEntry::new(0x15, 1, vec![InterType::Reg])?,
+            ),
+            (
+                "inc".to_string(),
+                OpcodeEntry::new(0x16, 1, vec![InterType::Reg])?,
+            ),
+            (
+                "dec".to_string(),
+                OpcodeEntry::new(0x17, 1, vec![InterType::Reg])?,
+            ),
+            (
+                "push".to_string(),
+                OpcodeEntry::new(0x18, 1, vec![InterType::Reg])?,
+            ),
+            (
+                "pop".to_string(),
+                OpcodeEntry::new(0x19, 1, vec![InterType::Reg])?,
+            ),
+            (
+                "call".to_string(),
+                OpcodeEntry::new(0x1a, 1, vec![InterType::Addr])?,
+            ),
+            ("ret".to_string(), OpcodeEntry::new(0x1b, 0, vec![])?),
+        ]);
+        Ok(OpcodeTable { table })
+    }
 
-    fn fetch_entry(&self, key: &str) -> Result<OpcodeEntry, AssemblyError> {}
+    pub fn fetch_entry(&self, key: &str) -> Result<&OpcodeEntry, AssemblyError> {
+        let entry = match self.table.get(key).ok_or("") {
+            Ok(entry) => entry,
+            Err(_) => {
+                return Err(AssemblyError {
+                    code: AssemblyErrorCode::UnrecognizedOpcode,
+                    reason: Some(format!("[ {key} ] is not a valid operation")),
+                    severity: Severity::Fatal,
+                })
+            }
+        };
+        Ok(entry)
+    }
 }
--- src/program_parser.rs
+++ src/program_parser.rs
@@ -1,4 +1,7 @@
-use crate::data::{AssemblyError, InterType, Severity};
+use crate::{
+    constant,
+    data::{AssemblyError, AssemblyErrorCode, InterType, OpcodeTable, Severity},
+};
 
 struct IntermediateObject {
     intertype: InterType,
@@ -9,7 +12,7 @@ struct IntermediateObject {
 }
 
 impl IntermediateObject {
-    fn from_mnemonic(s: &str) -> Result<Self, AssemblyError> {
+    fn from_mnemonic(s: &str, expected_type: &InterType) -> Result<Self, AssemblyError> {
         todo!()
     }
     fn to_bytes(&self) -> Vec<u8> {
@@ -17,17 +20,58 @@ impl IntermediateObject {
     }
 }
 
-struct IntermediateInstruction {
+pub struct IntermediateInstruction {
     objects: Vec<IntermediateObject>,
 }
 impl IntermediateInstruction {
-    fn parse_string(s: &str) -> Result<Self, AssemblyError> {
-        todo!()
-    }
+    fn parse_string(raw: &str, opcode_table: &OpcodeTable) -> Result<Self, AssemblyError> {
+        let mut objects = vec![];
+        let tokenized = tokenize_instruction(raw);
+        let opcode_str = &tokenized[0];
+        let operation = opcode_table.fetch_entry(opcode_str)?;
 
+        if tokenized.len() - 1 != operation.fields {
+            return Err(AssemblyError {
+                code: AssemblyErrorCode::IncorrectNumberOfOperands,
+                reason: Some(format!(
+                    "supplied [ {} ] operands when [ {} ] were expected",
+                    tokenized.len() - 1,
+                    operation.fields
+                )),
+                severity: Severity::Fatal,
+            });
+        }
+        for (i, operand) in tokenized.iter().enumerate() {
+            let expected_type = match operation.field_types.get(i){
+                Some(t) => t,
+                None => unreachable!("field count is already verified to match lengh, so element should always exist")
+            };
+            let object = IntermediateObject::from_mnemonic(operand, expected_type)?;
+            objects.push(object);
+        }
+        Ok(Self { objects })
+    }
     fn to_bytes(&self) -> Vec<u8> {
         todo!()
     }
 }
 
-pub fn parse_program(program_raw: String) -> Result<Vec<IntermediateInstruction>, AssemblyError> {}
+fn tokenize_instruction(s: &str) -> Vec<String> {
+    todo!()
+}
+
+pub fn parse_program(
+    program_raw: Vec<&str>,
+) -> Result<Vec<IntermediateInstruction>, AssemblyError> {
+    let opcode_table = OpcodeTable::build_table()?;
+    let mut program = vec![];
+    for raw_instruction in program_raw {
+        if raw_instruction.starts_with(constant::LABEL) {
+            continue;
+        }
+
+        let instruction = IntermediateInstruction::parse_string(raw_instruction, &opcode_table)?;
+        program.push(instruction);
+    }
+    Ok(program)
+}
